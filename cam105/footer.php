361f0b86cec99f3e88b9492432b0eaffbee3f6b1
Axel Minet <axel@cambridge105.fm>
2011-02-18 13:47:32 +0000

    added footer for gigx-sponsors-slides

---
+++ cam105/footer.php
@@ -0,0 +1,27 @@
+<?php
+/*
+File Description: Default Footer
+Built By: GIGX
+Theme Version: 0.5.9
+*/
+?>
+        </div><!-- end of container--> 
+      </div><!-- end of main -->
+    	<?php if ( is_active_sidebar( 'above_footer_widgets' ) ) : // Widgets Above Footer ?>
+      	<div id="above-footer-widgets">
+      		<?php dynamic_sidebar('above_footer_widgets'); ?>
+      	</div>  
+      <?php endif; ?>            
+      <div id="footer">
+      	<p class="pleft blacktext smallcaps">&copy; <?php bloginfo('show'); ?> <?php echo date("Y"); ?></p>
+      	<p class="pright blacktext smallcaps">Created by: <a href="http://gigx.co.uk/"><img src="<?php bloginfo('template_url'); ?>/images/gigx-logo36x11.png" width="36" height="11" alt="GIGX.co.uk" title="GIGX.co.uk"/></a></p>
+      </div>
+    	<?php if ( is_active_sidebar( 'below_footer_widgets' ) ) : // Widgets Below Footer ?>
+      	<div id="below-footer-widgets">
+      		<?php dynamic_sidebar('below_footer_widgets'); ?>
+      	</div>  
+      <?php endif; ?>       
+    </div> <!-- end of page--> 
+    <?php wp_footer(); ?>
+  </body>
+</html>
