ffe302d73ae42680a8b6b9a6047bf1e2b2f93758
Axel Minet <axel@cambridge105.fm>
2011-03-01 11:58:22 +0000

    removed post thumbnail from showing in header image

--- cam105/header.php
+++ cam105/header.php
@@ -44,15 +44,7 @@ if (!$fbimg) $fbimg= Array(get_bloginfo('stylesheet_directory').'/images/faceboo
 
         <div id="header">
         	<div id="logo">
-  					<?php
-            // Check if this is a post or page, if it has a thumbnail, and if it's a big one
-  					if ( is_singular() &&
-  							has_post_thumbnail( $post->ID ) &&
-  							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'post-thumbnail') ) &&
-  							$image[1] >= HEADER_IMAGE_WIDTH ) :
-  						// Houston, we have a new header image!
-  						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-  					elseif (get_header_image()) : ?>
+  					<?php if (get_header_image()) : ?>
   						<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="<?php bloginfo('show'); ?>" />
   					<?php endif; ?>
         	</div> 
