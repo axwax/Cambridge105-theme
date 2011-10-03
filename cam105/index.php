ddbade891c2d5aab31b585e32227450cb81c7803
Axel Minet <axel@cambridge105.fm>
2011-10-03 16:46:57 +0100

    cam105: added loop-schedule.php (jason's schedule) and changed index.php to load loop-postname.php template if exists

--- cam105/index.php
+++ cam105/index.php
@@ -4,14 +4,15 @@ File Description: Default Index Page
 Author: Axel Minet
 Theme Version: 0.5.11
 */
-define('CUSTOM_POST_TYPE',get_post_type()); 
+define('CUSTOM_POST_TYPE',get_post_type());
+if(CUSTOM_POST_TYPE=='page')$loop=$post->post_name;
+else $loop= CUSTOM_POST_TYPE;
 get_header();
 ?>
 
 	<div id="content">
-    <?php get_template_part( 'loop', CUSTOM_POST_TYPE ); ?>
+		<?php get_template_part( 'loop', $loop ); ?>
 	</div>
 
- 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
