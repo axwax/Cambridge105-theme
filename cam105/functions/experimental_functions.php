501e3551f00832a5d9f4786385318b65e8e4adfc
Axel Minet <axel@cambridge105.fm>
2012-04-05 17:50:34 +0100

    cam105: added gigx_search_form to experimental functions

--- cam105/functions/experimental_functions.php
+++ cam105/functions/experimental_functions.php
@@ -207,4 +207,18 @@ function gigx_remove_menus() {
 	//remove_submenu_page( 'edit.php', 'post_type=gigx_slide' );
 
 }
-                
+  
+#### custom search form ####
+function gigx_search_form( $form ) {
+
+    $form = '<form role="search" method="get" id="searchform" action="' . home_url( '/' ) . '" >
+    <div><label class="screen-reader-text" for="s">' . __('Search for:') . '</label>
+    <input type="text" value="' . get_search_query() . '" name="s" id="s" />
+    <input type="submit" id="searchsubmit" value="'. esc_attr__('Search') .'" />
+    </div>
+    </form>';
+
+    return $form;
+}
+
+add_filter( 'get_search_form', 'gigx_search_form' );              
