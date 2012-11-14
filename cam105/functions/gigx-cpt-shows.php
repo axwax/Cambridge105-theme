2fe31ca4fbf09b2982808fcb7b9e8a8be54aa208
Axel Minet <axel@cambridge105.fm>
2012-11-14 16:36:23 +0000

    cam105: added revisions back to shows cpt

--- cam105/functions/gigx-cpt-shows.php
+++ cam105/functions/gigx-cpt-shows.php
@@ -37,11 +37,10 @@ function gigx_cpt_shows()
     'menu_position' => 5,
     'menu_icon' => get_stylesheet_directory_uri() . '/images/shows16x16.png',
     'show_in_nav_menus' => true,
-    'supports' => array('title','editor','thumbnail','sticky','custom-fields'),
+    'supports' => array('title','editor','thumbnail','sticky','custom-fields','revisions'),
     'taxonomies' => array( 'post_tag')
   );
   add_action('admin_head', 'shows_icon');
-  //print_r(get_bloginfo('stylesheet_directory') . '/images/shows16x16.png');
   register_post_type('shows',$args);
 }
 
