350cf906594c8293510e3bf162fa7a260529c447
Axel Minet <axel@cambridge105.fm>
2012-12-19 20:59:48 +0000

    cam105: add fb app id, remove <div id="header"> (may need checking)

--- cam105/header.php
+++ cam105/header.php
@@ -38,6 +38,7 @@ add_action('wp_enqueue_scripts', 'load_external_jQuery'); // initiate the functi
 
   <head profile="http://gmpg.org/xfn/11">
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta property="fb:app_id" content="134459063259922" />
     <title><?php wp_title(''); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css" type="text/css" media="screen" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
@@ -64,7 +65,7 @@ add_action('wp_enqueue_scripts', 'load_external_jQuery'); // initiate the functi
         <div id="header" class="clearfix">
 	  <div id="header-container">  
 		<?php diag("BEGIN HEADER") ?>
-        <div id="header">
+
         	<div id="logo">
   				<a href="<?php echo home_url();?>"><?php
 					if (get_header_image()) : ?>
