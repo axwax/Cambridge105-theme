d99a3bdd0972ab03187df83b974e629aa55f8785
Axel Minet <axel@cambridge105.fm>
2013-01-18 15:34:17 +0000

    cam105: did some sorting out of loop-news

--- cam105/loop-news.php
+++ cam105/loop-news.php
@@ -39,22 +39,22 @@ function gigx_newspost ($news, $twocol_row = false, $breadcrumb = '', $show_img=
     foreach($news as $post) : setup_postdata($post);
 
 	$postID = $post->ID;
-	$img=wp_get_attachment_image_src (get_post_thumbnail_id($post->ID),'shows-image',false);
+	$img=wp_get_attachment_image_src (get_post_thumbnail_id($postID),'shows-image',false);
 	$excerpt = $post->post_excerpt;
 	$content = $post->post_content;
-	$title = get_the_title($post->ID);
-	$permalink = apply_filters('the_permalink', get_permalink($post->ID));
+	$title = get_the_title($postID);
+	$permalink = apply_filters('the_permalink', get_permalink($postID));
 	
-    
+   $title_link = $title;
 	if ($title) {
-	    if(isset($permalink)) $title = '<a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '">'.$title.'</a>';
-	}	    
+	    if(isset($permalink)) $title_link = '<a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '">'.$title.'</a>';
+	}    
 	if($img && $show_img){
 	    if(isset($permalink)){
-		$img_html = '<div class="shows-thumb alignleft"><a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '">' . get_show_image('shows-thumb') . '</a></div>';
-		$img_html = '<div class="wp-caption alignleft" style="width: 310px"><a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$p->post_title.'" title="'.$post->post_title.'"/></a><p class="wp-caption-text">'.$title.'</p></div>';
+		$img_html = '<div class="shows-thumb alignleft"><a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '">' . get_show_image('shows-thumb',$postID) . '</a></div>';
+		$img_html = '<div class="wp-caption alignleft" style="width: 310px"><a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$title.'" title="'.$title.'"/></a><p class="wp-caption-text">'.$title_link.'</p></div>';
 	    }     
-	    else $img_html = '<div class="wp-caption alignleft" style="width: 310px"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$p->post_title.'" title="'.$post->post_title.'"/><p class="wp-caption-text">'.get_the_title().'</p></div>';
+	    else $img_html = '<div class="wp-caption alignleft" style="width: 310px"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$title.'" title="'.$title.'"/><p class="wp-caption-text">'.$title_link.'</p></div>';
 	}    
 	if (function_exists('gigx_excerpt')){
 	  $content=gigx_excerpt ($content,$excerpt,false,500,$permalink,'more',True);
@@ -63,8 +63,8 @@ function gigx_newspost ($news, $twocol_row = false, $breadcrumb = '', $show_img=
 	else $out .= '<div class="' . implode(' ',get_post_class("clearfix")) . '" id="post-' . $postID . '">';
 	if ($breadcrumb) $out .= $breadcrumb;	
 	$out .= 	$img_html;
-	$out .= '	<h1 class="post-title">'.$title.'</h1>';
-	$out .= '	<span class="postdate">Posted by ' . get_the_author() . ' on '. get_the_time('jS F Y') .'</span>'; 
+	$out .= '	<h1 class="post-title">'.$title_link.'</h1>';
+	$out .= '	<span class="postdate">Posted by ' . get_the_author() . ' on '. get_the_time('jS F Y',$postID) .'</span>'; 
 	$out .= '	<div class="entry" style="padding-top:10px;">' . $content . '</div>';
 	$out .= '</div>'."\r\n";
     endforeach;
