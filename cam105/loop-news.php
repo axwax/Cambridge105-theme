4a2d7168ab09f7980b5b72b7a0eacfe0c52a4bc4
Axel Minet <axel@cambridge105.fm>
2013-01-31 17:06:00 +0000

    cam105: slightly improved news page

--- cam105/loop-news.php
+++ cam105/loop-news.php
@@ -4,101 +4,70 @@ File Description: The Loop for the main News page
 Author: Axel Minet
 Theme Version: 0.6.2
 */
+
+
+if ( function_exists('yoast_breadcrumb') ) $breadcrumb = yoast_breadcrumb('<div id="breadcrumbs">','</div>',false);	
+
+// get headline news
+$args = array('numberposts'=>3, 'offset' => 0,'orderby' => 'post_date', 'order'=>'DESC','post_status' => 'publish');    
+$args['category_name'] = 'local-news';
+$args['numberposts']   = $postcount = 1;
+$news=get_posts( $args );
+$out = gigx_newspost ($news, false, $breadcrumb);
+
+// get other news
+$args['category_name']='press-releases';
+$args['numberposts']=1;
+$news=get_posts( $args );
+$out .= '<div class="twocol clearfix">';
+$out .= gigx_newspost($news,1,false,false);
+
+
+$args['category_name']='station-news';
+$args['numberposts']=1;
+$news=get_posts( $args );
+$out .= gigx_newspost($news,2,false,false);
+$out .= "</div><!--end of twocol -->";
+?>            
+
+<div class="posts">
+    <?php echo $out ?>      
+</div><!-- end of posts div -->
+<?php
+function gigx_newspost ($news, $twocol_row = false, $breadcrumb = '', $show_img=true){
+    $out = '';
+    foreach($news as $post) : setup_postdata($post);
+
+	$postID = $post->ID;
+	$img=wp_get_attachment_image_src (get_post_thumbnail_id($postID),'shows-image',false);
+	$excerpt = $post->post_excerpt;
+	$content = $post->post_content;
+	$title = get_the_title($postID);
+	$permalink = apply_filters('the_permalink', get_permalink($postID));
+	
+   $title_link = $title;
+	if ($title) {
+	    if(isset($permalink)) $title_link = '<a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '">'.$title.'</a>';
+	}    
+	if($img && $show_img){
+	    if(isset($permalink)){
+		$img_html = '<div class="shows-thumb alignleft"><a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '">' . get_show_image('shows-thumb',$postID) . '</a></div>';
+		$img_html = '<div class="wp-caption alignleft" style="width: 310px"><a href="' . esc_url($permalink) . '" title="' . esc_attr($title) . '"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$title.'" title="'.$title.'"/></a><p class="wp-caption-text">'.$title_link.'</p></div>';
+	    }     
+	    else $img_html = '<div class="wp-caption alignleft" style="width: 310px"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$title.'" title="'.$title.'"/><p class="wp-caption-text">'.$title_link.'</p></div>';
+	}    
+	if (function_exists('gigx_excerpt')){
+	  $content=gigx_excerpt ($content,$excerpt,false,500,$permalink,'more',True);
+	}
+	if($twocol_row) $out .= '<div class="' . implode(' ',get_post_class(array('clearfix', 'twocol-content col_' . $twocol_row))) . '" id="post-' . $postID . '">';
+	else $out .= '<div class="' . implode(' ',get_post_class("clearfix")) . '" id="post-' . $postID . '">';
+	if ($breadcrumb) $out .= $breadcrumb;	
+	$out .= 	$img_html;
+	$out .= '	<h1 class="post-title">'.$title_link.'</h1>';
+	$out .= '	<span class="postdate">Posted by ' . get_the_author() . ' on '. get_the_time('jS F Y',$postID) .'</span>'; 
+	$out .= '	<div class="entry" style="padding-top:10px;">' . $content . '</div>';
+	$out .= '</div>'."\r\n";
+    endforeach;
+    return $out;
+}
 ?>
-    <div class="posts">
-        <?php 
-		
-        $args = array('numberposts'=>3, 'offset' => 0,'orderby' => 'post_date', 'order'=>'DESC','post_status' => 'publish');    
-        $args['category_name']='headline-news';
-        $args['numberposts']=1;
-        
-        $news=get_posts( $args );
-        //print_r($news);
-        foreach($news as $post) : setup_postdata($post); ?>
-          <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-            
-            <?php
-            $img=wp_get_attachment_image_src (get_post_thumbnail_id(get_the_ID()),'shows-image',false);
-            $excerpt = $post->post_excerpt;
-            $content = $post->post_content;
-            $link=get_page_link($page->ID);
-            if (function_exists('gigx_excerpt')){
-              $content=gigx_excerpt ($content,$excerpt,false,500,$link,'(more...)',True);
-            }
-            echo '<div class="wp-caption alignleft" style="width: 310px"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$p->post_title.'" title="'.$post->post_title.'"/><p class="wp-caption-text">'.get_the_title().'</p></div>';
-            echo '<h1 class="post-title">'.get_the_title().'</h1>';
-            echo '<span class="postdate">Posted on '. get_the_time('jS F Y') .'</span>'; ?> 
-            <div class="entry" style="padding-top:10px;">
-              <?php echo $content; ?>
-          	</div>
-          </div>
-
-        <?php endforeach; ?>            
-
-
-        <div class="twocol">
-            <?php
-            $args['category_name']='local-news';
-            $args['numberposts']=2;
-            
-            $news=get_posts( $args );
-            //print_r($news);
-            $count=0;
-            foreach($news as $post) : setup_postdata($post); 
-            $count++;
-            ?>
-              <div class="twocol-content col_<?php echo $count; ?>" id="post-<?php the_ID(); ?>">
-                
-                <?php
-                $img=wp_get_attachment_image_src (get_post_thumbnail_id(get_the_ID()),'shows-image',false);
-            		//print_r($img);
-                $excerpt = $post->post_excerpt;
-                $content = $post->post_content;
-                $link=get_page_link($page->ID);
-                if (function_exists('gigx_excerpt')){
-                  $content=gigx_excerpt ($content,$excerpt,false,300,$link,'(more...)',True);
-                }
-                echo '<div class="wp-caption" style="width: 310px"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$p->post_title.'" title="'.$post->post_title.'"/></div>';
-                echo '<h3 class="post-title">'.get_the_title().'</h3>';
-                echo '<span class="postdate">Posted on '. get_the_time('jS F Y') .'</span>'; ?> 
-                <div class="entry" style="padding-top:10px;">
-                  <?php echo $content; ?>
-              	</div>
-              </div>
-    
-            <?php endforeach; ?>            
-        </div>       
-
-        <div class="twocol">
-            <?php
-            $args['category_name']='external-news';
-            $args['numberposts']=8;
-            
-            $news=get_posts( $args );
-            //print_r($news);
-            $count=0;
-            foreach($news as $post) : setup_postdata($post); 
-            $count++;
-            ?>
-              <div class="twocol-content twocol-<?php echo $count; ?>" id="post-<?php the_ID(); ?>">
-                
-                <?php
-
-                $excerpt = $post->post_excerpt;
-                $content = $post->post_content;
-                $link=get_page_link($page->ID);
-                if (function_exists('gigx_excerpt')){
-                  $content=gigx_excerpt ($content,$excerpt,false,300,$link,'(more...)',True);
-                }
-				//echo "excerpt:$excerpt-content:$content";
-                //echo '<div class="wp-caption" style="width: 310px"><img src="'.$img[0].'" width="'.$img[1].'" height="'.$img[2].'" alt="'.$p->post_title.'" title="'.$post->post_title.'"/><p class="wp-caption-text">'.get_the_title().'</p></div>';
-                echo '<h5 class="post-title">'.get_the_title().'</h5>';
-                echo '<span class="postdate">Posted on '. get_the_time('jS F Y') .'</span>'; ?> 
-                <div class="entry" style="padding-top:10px;">
-                  <?php echo $content; ?>
-              	</div>
-              </div>
-    
-            <?php endforeach; ?>            
-        </div>  
-</div><!-- end of posts div -->  
